c4e6db80205309a13af0d4d4182349b44c6534a1
Alessio Benito Alterani <alessiobenito7@gmail.com>
2023-10-11 16:02:03 +0200

    Fixed convert_json.py and coco_test.py

--- yolact/data/santal_dataset/coco_test.py
+++ yolact/data/santal_dataset/coco_test.py
@@ -9,9 +9,9 @@ pylab.rcParams['figure.figsize'] = (8.0, 10.0)
 
 def show_images(n_img_to_show,imgIds):
     
+   
     img_ann=coco.loadImgs(imgIds)
     n_img=len(img_ann)
-    print(n_img)
     count=0
     result,rest = (n_img // n_img_to_show, n_img % n_img_to_show)
 
@@ -24,6 +24,7 @@ def show_images(n_img_to_show,imgIds):
         for i in range(count,count+img_to_load):
             ax =fig.add_subplot(n_img_to_show,1,i+1-count)
             I = Image.open(img_ann[i]['coco_url'])
+            print(img_ann[i]['coco_url'])
             ax.imshow(I)
             plt.axis('off')
             annIds = coco.getAnnIds(imgIds=imgIds[i], iscrowd=None)
@@ -31,22 +32,31 @@ def show_images(n_img_to_show,imgIds):
             coco.showAnns(anns,draw_bbox=True)
         
         plt.show()
+      
         count+=img_to_load
 
-def sanity_check(imgIds):
+def structure_check(imgIds):
     annIds=coco.getAnnIds(imgIds, iscrowd=None)
     anns = coco.loadAnns(annIds)
-
+    errors=0
     for ann in anns:
-       assert len(ann['segmentation']) >0," segmentation is empty"
+       assert len(ann['segmentation']) >0,"segmentation is empty"
+       assert ann['area']!=0,f"area is 0 {ann}"
        for elem in ann['segmentation']:
-           assert len(elem)>2, "number of border points <2"
+           try:
+                assert len(elem)>2, f"number of border points <2, {elem}"
+           except(AssertionError):
+               errors+=1
+
+    if errors!=0:
+        print(f"Total border points <2 is {errors}")
+    
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
     '--img-to-show',
-    default=3,
+    default=1,
     type=int,
     help = 'number of images to show in a iteration'
 )
@@ -65,6 +75,12 @@ parser.add_argument(
     help = 'show annotations on images'
 )
 
+parser.add_argument(
+    '--object',
+    default="santal_ace",
+    help = 'object whose annotations will be shown'
+)
+
 opt = parser.parse_args()
 n_img_to_show=opt.img_to_show
 annFile=opt.ann_file
@@ -77,12 +93,12 @@ print('COCO categories: \n{}\n'.format(' '.join(nms)))
 
 nms = set([cat['supercategory'] for cat in cats])
 print('COCO supercategories: \n{}'.format(' '.join(nms)))
-catIds = coco.getCatIds(catNms=['002_master_chef_can'])
+catIds = coco.getCatIds(catNms=[opt.object])
 imgIds = coco.getImgIds(catIds=catIds)
 
 #check if the annotations are properly filled
-sanity_check(imgIds)
-
+structure_check(imgIds)
+print(f"total number of images {len(imgIds)}")
 #shows image annotations
 if opt.show_img:
     show_images(opt.img_to_show,imgIds)
